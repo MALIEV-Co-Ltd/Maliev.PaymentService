b04b40e8def1d29f6e92ea3e077d835387ed97d5
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2025-12-20 00:00:22 +0700

    refactor: update to new MessagingContracts.Generated namespace

--- Maliev.PaymentService.Api/Controllers/TestController.cs
+++ Maliev.PaymentService.Api/Controllers/TestController.cs
@@ -1,4 +1,4 @@
-using Maliev.MessagingContracts;
+using Maliev.MessagingContracts.Generated;
 using Maliev.PaymentService.Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
--- Maliev.PaymentService.Infrastructure/Services/PaymentService.cs
+++ Maliev.PaymentService.Infrastructure/Services/PaymentService.cs
@@ -5,7 +5,7 @@ using Maliev.PaymentService.Core.Interfaces;
 using Maliev.PaymentService.Infrastructure.Providers;
 using Maliev.PaymentService.Infrastructure.Resilience;
 using Microsoft.Extensions.Logging;
-using Maliev.MessagingContracts;
+using Maliev.MessagingContracts.Generated;
 
 namespace Maliev.PaymentService.Infrastructure.Services;
 
